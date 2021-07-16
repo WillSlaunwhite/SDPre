aa03665cda16913d25b57d78cd81b12b910fa8f2
Will Slaunwhite <williamslaunwhite@gmail.com>
2021-07-16 09:43:53 -0600

    Objective 1 - Objective 5

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    name = "Will Slaunwhite";
+    age = 25;
+    state = "Ontario, Canada";
+
+    System.out.println("Name: "+ name + " " + "Age: " + age + " " + "State: "+state);
+  }
+}
