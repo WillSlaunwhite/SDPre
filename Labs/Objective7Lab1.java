f19a28fa1e44f7d2fb210cffba1635684ba2c22b
Will Slaunwhite <williamslaunwhite@gmail.com>
2021-07-17 21:50:27 -0600

    Objective 7

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,17 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    do{
+      System.out.println(counter);
+      counter-=1;
+    } while(counter > 0);
+
+      /*
+      Your solution goes here
+      */
+
+      //counter = counter - 1; //this makes we count down
+
+  }
+}
